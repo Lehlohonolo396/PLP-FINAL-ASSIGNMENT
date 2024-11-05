2c83244811c681a6312677c1041a8e2aefebd1fd
Lehlohonolo396 <tlhogi928@gmail.com>
2024-11-05 18:25:44 +0200

    expense_tracker

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const { Sequelize } = require('sequelize');
+
 
 const app = express();
 app.use(cors());
